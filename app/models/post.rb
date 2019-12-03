ea871ad961f721390150117488d337200e216d17
Karen V <karenvo@usc.edu>
2019-12-03 03:57:42 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: {minimum: 2}
   validates :category, presence: true
+  validates :content, length: {minimum: 2}
 end
